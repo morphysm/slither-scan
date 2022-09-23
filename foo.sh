9d0472df1351fc17422c85ad4745e4ee198dc8b5
GreyXor <greyxor@protonmail.com>
2022-09-23 12:22:32 +0200

    add sh example

--- foo.sh
+++ foo.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-find ./ -mtime -7 -name '*.sol' -print0 | while IFS= read -r -d '' file
+# [filepath]: solidityVersion
+declare -A mapFileSolVersion
+
+# Extract the correct version from every *.sol file and put it in mapFileSolVersion array
+find ./ -name '*.sol' -print0 | while IFS= read -r -d '' file
 do
-  echo "$file"
+  pragmas=$(grep "pragma" $file)
+  
+  versions=$(echo $pragmas | grep -Eo '[0-9]\.[0-9]+')
+  echo $file contains $versions
 done
 
+# Run sol-select and slither for each file
+for i in "${!array[@]}"
+do
+echo i 
+echo "${array[$i]}"
+  #solc-select install "${array[$i]}"
+  #solc-select use "${array[$i]}"
+  #slither "$i" --json /tmp/json
+done
